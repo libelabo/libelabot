ea43f0209439ac764d59813a5be4b38de1506833
june29 <june29x@gmail.com>
2008-07-30 15:23:45 +0900

    Added more channels\nFixed regexp in title fetcher

--- libelabot.rb
+++ libelabot.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'net/irc'
 require 'yaml'
 require 'open-uri'
+require 'kconv'
 
 $KCODE = 'u'
 
@@ -15,6 +16,8 @@ class SimpleClient < Net::IRC::Client
   def on_rpl_welcome(m)
     puts '\t\trpl_welcome'
     post JOIN, "#libelabo"
+    post JOIN, "#june29"
+    post JOIN, "#kei-s"
   end
 
   def on_connected
@@ -38,8 +41,8 @@ class SimpleClient < Net::IRC::Client
         content_type = response.content_type
         charset = response.charset
         s = ""
-        if doc =~ /<title>\s*(.*)\s*<\/title>/
-          s += $1 + " "
+        if doc =~ /<title>\s*(.*)\s*<\/title>/i
+          s += $1.toutf8 + " "
         end
         s += [content_type, charset].join(',')
         post NOTICE, channel, s
